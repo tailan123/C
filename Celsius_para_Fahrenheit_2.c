54dcfe9853d2ff66a6674694d4ccfbf778fc6ea1
tailan123 <102363984+tailan123@users.noreply.github.com>
2022-04-02 23:44:40 -0300

    Add files via upload

---
+++ Celsius_para_Fahrenheit_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    float celsius, fahrenheit;
+        printf("Graus celsius \n");
+        scanf("%f", &celsius);
+    fahrenheit = (celsius * 9/5)+32;
+        printf("%.1f Farenheit",fahrenheit);
+
+
+
+
+return 0;
+}
